98fe760342b5ef22c9573d65dc3c489b0a0836b6
Gabriel <rhernadnez028@west-mec.org>
2019-09-19 18:25:52 -0700

    Restarted Project01
    Commented out my old code to refer back to

--- Projects/Project_Lab/www/scripts.js
+++ Projects/Project_Lab/www/scripts.js
@@ -8,18 +8,17 @@
 
 "use strict";
 
-// Global variables
 var httpRequest = false;
 var entry = "^IXIC";
 
 function getRequestObject() {
-    try {
+    try{
         httpRequest = new XMLHttpRequest();
     } catch (requestError) {
         return false;
     }
     alert(httpRequest);
-    return httpRequest;
+    return
 }
 
 function stopSubmission(evt) {
@@ -29,19 +28,78 @@ function stopSubmission(evt) {
     } else {
         evt.returnValue = false;
     }
-
-    getQuote();
 }
 
-function getQuote() {
-    alert("getQuote()");
-    if (document.getElementsByTagName("input")[0].value) {
-        entry = document.getElementsByTagName("input")[0].value;
-    }
-    if (!httpRequest) {
-        httpRequest = getRequestObject();
-    }
+if (window.addEventListener) {
+    window.addEventListener("load", getRequestObject, false);
+} else if (window.attachEvent){ 
+    window.attachEvent("onload", getRequestObject);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Global variables
+// var httpRequest = false;
+// var entry = "^IXIC";
+
+// var form = document.getElementsByTagName("form")[0];
+// if (form.addEventListener) {
+//     form.addEventListener("submit", stopSubmission, false);
+//     window.addEventListener("load", getQuote, false);
+// } 
+// else if (form.attachEvent) {
+//     form.attachEvent("onsubmit", stopSubmission);
+//     window.attachEvent("onload", getQuote);
+// }
+
+// function stopSubmission(evt) {
+//     alert("stopSubmission()");
+//     if (evt.preventDefault) {
+//         evt.preventDefault();
+//     } else {
+//         evt.returnValue = false;
+//     }
+//     getQuote();
+// }
+
+// function getQuote() {
+     //alert("getQuote()");
+//     if (document.getElementsByTagName("input")[0].value) {
+//         entry = document.getElementsByTagName("input")[0].value;
+//     }
+//     if (!httpRequest) {
+//         httpRequest = getRequestObject();
+//     }
+
+//     httpRequest.abort();
+//     httpRequest.open("get", "StockCheck.php?t=" + entry, true);
+//     httpRequest.send(null);
+
+//     httpRequest.onreadystatechange = displayData;
+// }
+
+// function displayData() {
+//     if (httpRequest.readyState === 4 && httpRequest.status === 200) { 
+//         var stockReslults = httpRequest.responseText;
+//         console.log(stockReslults);
+//     }
+// }
 
 // if (window.addEventListener) {
 //     window.addEventListener("load", getRequestObject, false)
@@ -49,11 +107,12 @@ function getQuote() {
 //     window.attachEvent("onload", getRequestObject);
 // }
 
-var form = document.getElementsByTagName("form")[0];
-if (form.addEventListener) {
-    form.addEventListener("submit", stopSubmission, false);
-    window.addEventListener("load", getQuote, false);
-} else if (form.attachEvent) {
-    form.attachEvent("onsubmit", stopSubmission);
-    window.attachEvent("onload", getQuote);
-}
+// var form = document.getElementsByTagName("form")[0];
+// if (form.addEventListener) {
+//     form.addEventListener("submit", stopSubmission, false);
+//     window.addEventListener("load", getRequestObject, false);
+// } 
+// else if (form.attachEvent) {
+//     form.attachEvent("onsubmit", stopSubmission);
+//     window.attachEvent("onload", getRequestObject);
+// }
